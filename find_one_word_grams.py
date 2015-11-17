5340a27efa162c0fdc15201bdec2ba8113fd841c
Shenglan Qiao <shenglan@corn09.stanford.edu>
2015-11-16 16:06:49 -0800

    modified script to save a list of dictionary in sparse format

--- find_one_word_grams.py
+++ find_one_word_grams.py
@@ -15,40 +15,58 @@ import pandas as pd
 import numpy as np
 import time
 import os
+import pickle
+import csv
 ##############################################################################
 # Code
 ##############################################################################
 
+all_lg = ['bs','hr','sr']
 
 def find_one_grams(line):
     words = line.split()
     lg = words[-1]
+    if lg == all_lg[0]:
+        lg = 1
+    elif lg == all_lg[1]:
+        lg = 2
+    elif lg== all_lg[2]:
+        lg = 3
+    
     one_grams = words[:-1]
     d = {}
     for gram in one_grams:
         if gram in d.keys():
-            a = np.array([1],dtype = np.int8)
-            d[gram] += a[0]
+            #only recording 0 and 1 for wether a word is present or not
+            pass
+#             a = np.array([1],dtype = np.int8)
+#             d[gram] += a[0]
         else:
             a = np.array([1],dtype=np.int8)
             d.update({gram:a[0]})
     return d,lg
 
 
-f=open(os.getcwd()+'/2500_train.txt','r')
+f=open(os.getcwd()+'/partial_train_data/54k_sws_train.txt','r')
 # f=open(os.getcwd()+'/test_test.txt','r')
 
 tic = time.clock()
 
-dds = pd.DataFrame()
+# dds = pd.DataFrame()
+dds= []
 lgs = []
 for line in f:
     d,lg = find_one_grams(line)
-    with open(os.getcwd()+'/lg_labels_2500.txt','a') as lg_f:
-        lg_f.write(lg+'\n')
-    dds = dds.append(d,ignore_index=True)
-    
-dds.to_csv(path_or_buf=os.getcwd()+'/one_word_grams_2500_train.txt',sep = ' ',index= False)
+    with open(os.getcwd()+'/processed_training/sws_labels_54k.csv','a') as lg_f:
+        csvwriter = csv.writer(lg_f, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow([lg])
+#     dds = dds.append(d,ignore_index=True)
+    dds.append(d)
+with open(os.getcwd()+'/processed_training/sws_54k.pkl', 'wb') as fp:
+    pickle.dump(dds, fp)
+
+# dds.to_csv(path_or_buf=os.getcwd()+'/one_word_grams_2500_train.txt',sep = ' ',index= False)
 toc = time.clock()
 print "total time for process is %.2f seconds." % (toc-tic)
 
