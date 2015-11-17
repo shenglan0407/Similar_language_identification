e7e50c5ceed5b19c73a5b41a3b706ebf00a7de81
Shenglan Qiao <shenglan@corn29.stanford.edu>
2015-11-16 17:33:41 -0800

    labeling word in vocabulary by number 1,2,3... and saving a vocab file

--- find_one_word_grams.py
+++ find_one_word_grams.py
@@ -22,6 +22,7 @@ import csv
 ##############################################################################
 
 all_lg = ['bs','hr','sr']
+vocab=[]
 
 def find_one_grams(line):
     words = line.split()
@@ -42,12 +43,20 @@ def find_one_grams(line):
 #             a = np.array([1],dtype = np.int8)
 #             d[gram] += a[0]
         else:
-            a = np.array([1],dtype=np.int8)
-            d.update({gram:a[0]})
+            if gram in vocab:
+                number = int(np.where(np.array(vocab)==gram)[0][0]+1)
+                a = np.array([1],dtype=np.int8)
+                d.update({number:a[0]})
+            else:
+                vocab.append(gram)
+                number = len(vocab)
+                a = np.array([1],dtype=np.int8)
+                d.update({number:a[0]})
+     
     return d,lg
 
 
-f=open(os.getcwd()+'/partial_train_data/54k_sws_train.txt','r')
+f=open(os.getcwd()+'/partial_train_data/sws_train_5400.txt','r')
 # f=open(os.getcwd()+'/test_test.txt','r')
 
 tic = time.clock()
@@ -57,12 +66,18 @@ dds= []
 lgs = []
 for line in f:
     d,lg = find_one_grams(line)
-    with open(os.getcwd()+'/processed_training/sws_labels_54k.csv','a') as lg_f:
+    with open(os.getcwd()+'/processed_training/sws_labels_5400k.csv','a') as lg_f:
         csvwriter = csv.writer(lg_f, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csvwriter.writerow([lg])
-#     dds = dds.append(d,ignore_index=True)
     dds.append(d)
+
+with open(os.getcwd()+'/processed_training/sws_train_5400_vocab.csv','a') as lg_f:
+        csvwriter = csv.writer(lg_f, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for this_w in vocab:
+            csvwriter.writerow(w)
+
 with open(os.getcwd()+'/processed_training/sws_54k.pkl', 'wb') as fp:
     pickle.dump(dds, fp)
 
